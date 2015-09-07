bc0ae35a6f93736f89886036c0ef9bd85ded69ce
Roman Belik <dabonka@yandex.ru>
2015-09-07 21:57:06 +0400

    just trying solve the problem with controller and view

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,14 +1,15 @@
 class CardsController < ApplicationController
 
-  def list
+  def index
     @cards = Card.all
   end
   
   def show
-    @cards = Card.find(params[:id])
+    @card = Card.find(params[:id])
   end
   
   def new
   end
     
+	
 end
