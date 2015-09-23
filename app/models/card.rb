85f0d7b861459245b008d57c241fd892571a1c1b
Roman Belik <dabonka@yandex.ru>
2015-09-23 12:42:18 +0400

    some changes in validation in model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,16 @@
+class EqualValidator < ActiveModel::Validator
+  def validate(card)
+    if card.original_text.downcase == card.translated_text.downcase
+      translated_text.errors[:base] << "The values of original text and translated text should be different"
+    end
+  end
+end
+
 class Card < ActiveRecord::Base
+  validates :original_text, :translated_text, :review_date, presence: true
+  validates_with EqualValidator
+end
+
+def set_review_date
+  review_date = DateTime.now = 3.days
 end
