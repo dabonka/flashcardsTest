7ea691d3e4018c504c238fcebdc3c936b35aa7ed
Roman Belik <dabonka@yandex.ru>
2015-09-25 10:12:54 +0400

    some changes in controller and model

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,23 +1,30 @@
 class CardsController < ApplicationController
 
-  def index
-    @cards = Card.all
-  end
+    def index
+      @cards = Card.all
+    end
   
-  def show
-    @card = Card.find(params[:id])
-  end
+	def show
+	  @card = Card.find(params[:id])
+	end
   
-  def new
-    @card = Card.new
-  end
+	def new
+	  @card = Card.new
+	end
   
-def create
-  @card = Card.new({:original_text => params[:original], :translated_text => params[:translate]})
- 
-  @card.save
-  redirect_to @card
-end
+	def create
+	  @card = Card.new(strong_params)
+	 
+	  @card.save
+	  redirect_to @card
+	end
     
+	def set_review_date
+	  review_date = DateTime.now + 3.days
+	end
+
+	def strong_params
+	  params.require(:@card).permit(:original_text, :translated_text, :review_date)
+	end
 	
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,22 +1,19 @@
-class Card < ActiveRecord::Base
-  validates :original_text, :translated_text, :review_date, presence: true
-end
-
 class EqualValidator < ActiveModel::Validator
   def validate(card)
-    if ((card !== nil) && (card.original_text !== nil) && (card.translated_text !== nil)) then
-      if card.original_text.downcase == card.translated_text.downcase
-        translated_text.errors[:base] << "The values of original text and translated text should be different"
-      end
-    else
-	end
+    if ((card != nil) && (card.original_text != nil) && (card.translated_text != nil)) 
+	  if card.original_text.downcase == card.translated_text.downcase
+		 translated_text.errors[:base] << "The values of original text and translated text should be different"
+	  end
+    end
   end
 end
 
 class Card < ActiveRecord::Base
+  validates :original_text, :translated_text, :review_date, presence: true
   validates_with EqualValidator
 end
 
 def set_review_date
   review_date = DateTime.now + 3.days
 end
+
