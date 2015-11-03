b62bfaab0b192c1a3d09ea685fb0ed73aaa79b74
Roman Belik <dabonka@yandex.ru>
2015-11-03 20:24:27 +0400

    final changes

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -9,26 +9,41 @@ class CardsController < ApplicationController
 	end
   
 	def new
-  logger.debug "def new from debug"
-  logger.info "def new from info"
 	  @card = Card.new
 	end
-  
+
+		def edit
+			@card = Card.find(params[:id])
+		end
+
 	def create
-  logger.debug "def create from debug"
-  logger.info "def create from info"
 	  @card = Card.new(cards_params)
 	 
 	  @card.save!
 	  redirect_to @card
 	end
 
+	def update
+		@card = Card.find(params[:id])
+
+		if @card.update(cards_params)
+			redirect_to @card
+		else
+			render 'edit'
+		end
+	end
+	
+	def destroy
+		@card = Card.find(params[:id])
+		@card.destroy
+ 
+		redirect_to cards_path
+	end
+
   private
     
 
 	def cards_params
-  logger.debug "def cards_params from debug"
-  logger.info "def cards_params from info"
 	  params.require(:card).permit(:original_text, :translated_text, :review_date)
 	end
 	
