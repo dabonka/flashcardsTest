f8863cd8a70b2a3c12c3a55374048e3fd9e42789
Roman Belik <dabonka@yandex.ru>
2015-11-03 23:29:24 +0400

    final changes2

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,31 +1,25 @@
 class EqualValidator < ActiveModel::Validator
   def validate(card)
-  Rails.logger.info "validates"
-    if ((card != nil) && (card.original_text != nil) && (card.translated_text != nil) && (card.review_date != nil)) 
-	  if card.original_text.downcase == card.translated_text.downcase
-		 translated_text.errors[:base] << "The values of original text and translated text should be different"
-	  end
-    end
+    # if ((card != nil) && (card.original_text != nil) && (card.translated_text != nil) && (card.review_date != nil))
+      if card.original_text.downcase == card.translated_text.downcase
+       translated_text.errors[:base] << "The values of original text and translated text should be different"
+	    end
+    # end
   end
 end
 
 class Card < ActiveRecord::Base
 
-def set_review_date
-  self.review_date = DateTime.now + 3.days
-  logger.debug "def set_review_date from debug"
-  logger.info "def set_review_date from info"
-end
+  def set_review_date
+    self.review_date = DateTime.now + 3.days
+  end
 
-  logger.debug "before_validation :set_review_date from debug"
-  logger.info "before_validation :set_review_date from info"
-  before_validation :set_review_date
+    before_validation :set_review_date
 
+    validates :original_text, :translated_text, :review_date, presence: true
 
+    validates_with EqualValidator
 
-  validates :original_text, :translated_text, :review_date, presence: true
-  
-  validates_with EqualValidator
 end
 
 
