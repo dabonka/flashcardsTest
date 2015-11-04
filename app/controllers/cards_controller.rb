20144cb7179b27107a4a250af765aec50d1e6114
Roman Belik <dabonka@yandex.ru>
2015-11-04 15:40:26 +0400

    make a good view for code

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,8 +1,8 @@
 class CardsController < ApplicationController
 
-    def index
-      @cards = Card.all
-    end
+	def index
+	  @cards = Card.all
+	end
   
 	def show
 	  @card = Card.find(params[:id])
@@ -12,37 +12,33 @@ class CardsController < ApplicationController
 	  @card = Card.new
 	end
 
-		def edit
-			@card = Card.find(params[:id])
-		end
+	def edit
+	  @card = Card.find(params[:id])
+	end
 
 	def create
 	  @card = Card.new(cards_params)
-	 
 	  @card.save!
 	  redirect_to @card
 	end
 
 	def update
-		@card = Card.find(params[:id])
-
-		if @card.update(cards_params)
-			redirect_to @card
+	  @card = Card.find(params[:id])
+	    if @card.update(cards_params)
+		  redirect_to @card
 		else
-			render 'edit'
+		  render 'edit'
 		end
 	end
 	
 	def destroy
-		@card = Card.find(params[:id])
-		@card.destroy
- 
-		redirect_to cards_path
+	  @card = Card.find(params[:id])
+	  @card.destroy
+ 	  redirect_to cards_path
 	end
 
   private
     
-
 	def cards_params
 	  params.require(:card).permit(:original_text, :translated_text, :review_date)
 	end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,10 +1,10 @@
 class EqualValidator < ActiveModel::Validator
   def validate(card)
-    # if ((card != nil) && (card.original_text != nil) && (card.translated_text != nil) && (card.review_date != nil))
-      if card.original_text.downcase == card.translated_text.downcase
-       translated_text.errors[:base] << "The values of original text and translated text should be different"
-	    end
-    # end
+    if ((card != nil) && (card.original_text != nil) && (card.translated_text != nil) && (card.review_date != nil)) 
+	  if card.original_text.downcase == card.translated_text.downcase
+        translated_text.errors[:base] << "The values of original text and translated text should be different"
+	  end
+    end
   end
 end
 
@@ -14,12 +14,10 @@ class Card < ActiveRecord::Base
     self.review_date = DateTime.now + 3.days
   end
 
-    before_validation :set_review_date
-
-    validates :original_text, :translated_text, :review_date, presence: true
-
-    validates_with EqualValidator
-
+  before_validation :set_review_date
+  validates :original_text, :translated_text, :review_date, presence: true
+  validates_with EqualValidator
+  
 end
 
 
