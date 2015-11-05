d5ea15fd65d6fc78c37510c2a6f20217e00056a7
Roman Belik <dabonka@yandex.ru>
2015-11-05 16:25:35 +0400

    Seeds.rb was changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,18 @@
+require "nokogiri"
+require "open-uri"
+
+page = Nokogiri::HTML(open("http://lingvotutor.ru/1500-samyx-upotreblyaemyx-slov-k-toefl"))
+
+word_list = []
+
+page.search("th.column-1")[0].each do |row|
+  original = row.css('th.column-1')[0].text
+  translated = row.css('th.column-3')[0].text
+  word_list << Hash[original, translated]
+end
+# word_list.each do |original, translated|
+#   Card.create( original_text: original, translated_text: translated )
+# end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
