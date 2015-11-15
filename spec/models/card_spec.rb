6b49cc086d99af2ffe5f4ef358a3b981bae26ab8
Roman Belik <dabonka@yandex.ru>
2015-11-16 01:44:12 +0400

    add tests

---
+++ spec/models/card_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe "Card" do
+  
+  describe "set_review_date" do
+    it "set review date to now + 3 days" do
+      card = Card.new(original_text: "Olala", translated_text: "hause")
+      card.save!
+      expect(card.set_review_date == (Date.current + 3.days) ).to be true
+    end
+  end
+
+ describe "check_translation" do
+	  it "allow using camelcase" do
+      card = Card.new(translated_text: "hause")
+      expect(card.check_translation("Hause")).to be true
+    end
+
+    it "allow using spaces" do
+      card = Card.new(translated_text: " hause ")
+      expect(card.check_translation("hause")).to be true
+    end
+  end
+end
