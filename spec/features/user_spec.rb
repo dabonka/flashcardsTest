3b50588e10f060a6a058e0a9893cfec36cc612f5
Roman Belik <dabonka@yandex.ru>
2015-12-07 13:55:16 +0400

    add pending tests

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -4,23 +4,39 @@ require 'capybara/rspec'
 
 describe  "Users", :type => :feature do
 
-  it  "Create new user" do
-    visit new_user_path
-    fill_in 'user_email', :with => 'example@example.com'
-    fill_in 'user_password', :with => '123123321321'
-    fill_in 'user_password_confirmation', :with => '123123321321'
-    click_button 'Create User'
-    expect(page).to have_content 'Email'
+  describe "Create new user" do
+
+    it  "Create new user with invalid email"
+    it  "Create new user with blank email"
+    it  "Create new user with too short password"
+    it  "Create new user without password"
+
+    it  "Create new user with correct data" do
+      visit new_user_path
+      fill_in 'user_email', :with => 'example@example.com'
+      fill_in 'user_password', :with => '123123321321'
+      fill_in 'user_password_confirmation', :with => '123123321321'
+      click_button 'Create User'
+      expect(page).to have_content 'Email'
+    end
   end
 
-   it  "Login user" do
-     user = FactoryGirl.create(:user)
-     visit login_path
-     fill_in 'email', :with => user.email
-     fill_in 'password', :with => 'password'
-     check 'remember'
-     click_button 'Login'
-     expect(page).to have_content 'Login successful'
-     end
+  describe  "Login user" do
+
+    it  "Login user with invalid email"
+    it  "Login user with blank email"
+    it  "Login user with invalid password"
+    it  "Create new without password"
+
+    it "Login user with correct data" do
+      user = FactoryGirl.create(:user)
+      visit login_path
+      fill_in 'email', :with => user.email
+      fill_in 'password', :with => 'password'
+      check 'remember'
+      click_button 'Login'
+      expect(page).to have_content 'Login successful'
+    end
+  end
 
 end
