15334f8d3c68955221d8963509f7f9bd05f71ff7
Roman Belik <dabonka@yandex.ru>
2015-12-21 15:35:27 +0400

    more certain gitignore and eliminate unuseful tests

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -6,9 +6,9 @@ FactoryGirl.define do
   factory :card do
     original_text "Word"
     translated_text "Slovo"
-    avatar_content_type "image/jpeg"
-    avatar_file_name "Sky.jpg"
-    avatar_file_size 62293
+    # avatar_content_type "image/jpeg"
+    # avatar_file_name "Sky.jpg"
+    # avatar_file_size 62293
     association :user, factory: :user
     #  review_date Date.current
     #  id 1000
